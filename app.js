607bc08ef6a313282cabe7a62cf29662eb74820b
Jedezese <jedezese04@gmail.com>
2021-11-07 19:27:18 +0700

    Mostly ready

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const multer = require('multer')
+const fs = require('fs')
+const mongoose = require('mongoose')
+
+// Default setup
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'uploads')))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// Multer setup
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/image')
+    },
+    filename: (req, file, cb) => {
+        let filename = Date.now() + file.originalname
+        cb(null, filename)
+    }
+})
+
+// Mongoose setup
+mongoose.connect('mongodb://localhost:27017/multer-files-uploading', () => console.log('Database connected!'))
+
+// Mongoose Schema
+let imageSchema = mongoose.Schema({
+    name: String
+})
+let Image = mongoose.model('images', imageSchema)
+
+// Router
+// '/'
+app.get('/', (req, res) => {
+    Image.find().sort({_id: -1}).exec((err, result) => {
+        res.render('index', {images: result})
+    })
+})
+
+// '/add'
+const upload = multer({
+    storage: storage,
+    // limits: { fileSize: 5000000 },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+          cb(null, true);
+        } else {
+          req.fileValidationError = "Forbidden extension"
+          return cb(null, false, req.fileValidationError)
+        }
+    }
+})
+
+app.post('/add', upload.any('images'), function (req, res) {
+    if(req.fileValidationError) {
+        res.redirect('/')
+    } else {
+        req.files.forEach((file) => {
+            let saveImage = new Image({
+                name: file.filename
+            })
+            saveImage.save()
+        })
+        res.redirect('/')
+    }
+})
+
+// '/delete'
+app.get('/delete/:name', (req, res) => {
+    let fileName = req.params.name
+    let filePath = `./uploads/image/${fileName}`
+    fs.unlink(filePath, err => console.log(err))
+    Image.remove({name: fileName}).exec((err, result) => {
+        if(err) console.log(err)
+        console.log(result)
+    })
+    res.redirect('/')
+})
+
+app.listen(3000, () => console.log("Server is running on http://localhost:3000"))
